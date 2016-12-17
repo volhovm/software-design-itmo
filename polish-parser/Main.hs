baaf8cc0eb21c9c83f59f163b15866457c4236dd
Mikhail Volkhov <volhovm.cs@gmail.com>
2016-12-17 10:37:44 +0300

    Add tokenizer

--- polish-parser/Main.hs
+++ polish-parser/Main.hs
@@ -1,4 +1,48 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE OverloadedStrings     #-}
+
 module Main where
 
+import           Data.Bifunctor (first)
+import           Data.Char      (isNumber, isSpace)
+import           Data.List      (span)
+import           Universum
+
+type String = [Char]
+
+class Visitor m a where
+    visit :: a -> m ()
+
+----------------------------------------------------------------------------
+-- Tokenizer
+----------------------------------------------------------------------------
+
+data PreToken = PLeft | PRight | PDiv | PMul | PPlus | PMinus | PNum Int
+              deriving (Show)
+
+tokenize :: String -> [PreToken]
+tokenize [] = []
+tokenize input0 = parsed ++ tokenize (other ++ afterSpace)
+  where
+    strip = span (not . isSpace) . dropWhile isSpace
+    (input,afterSpace) = strip input0
+    (parsedToken,other) = parseChar input
+    parsed :: [PreToken]
+    parsed = bool [parsedToken] [] $ null input
+    parseChar :: String -> (PreToken,String)
+    parseChar (c:other) | c `elem` ("+-*/()"::String) =
+        let curToken = case c of
+                '+' -> PPlus
+                '-' -> PMinus
+                '*' -> PMul
+                '/' -> PDiv
+                '(' -> PLeft
+                ')' -> PRight
+                _   ->panic "Parse error"
+        in (curToken,other)
+    parseChar s | not . null $ (reads s :: [(Int,String)]) =
+        first PNum $ fromMaybe (panic "failed to parse int") $ head $ reads s
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = putText "Hello, Haskell!"
